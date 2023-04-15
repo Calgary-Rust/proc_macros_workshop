58906b54976942da417fe4fdb52b71aa61b7b3a6
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-04-15 14:51:35 -0600

    Provided a skeleton for the implementation of ValidationErrorType derive macro

--- validator_extra_macros/src/lib.rs
+++ validator_extra_macros/src/lib.rs
@@ -1,1 +1,6 @@
+use proc_macro::TokenStream;
 
+#[proc_macro_derive(ValidationErrorType, attributes(validation_error_type))]
+pub fn derive_validation_error_type(_input: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
