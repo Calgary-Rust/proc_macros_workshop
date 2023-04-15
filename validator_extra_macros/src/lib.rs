a6226c5c5de32f1e38691abfa035eaaf3da54d9b
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-04-15 14:53:42 -0600

    Demonstrated the need for proc-macro2 and *-core crate

--- validator_extra_macros/src/lib.rs
+++ validator_extra_macros/src/lib.rs
@@ -4,3 +4,19 @@ use proc_macro::TokenStream;
 pub fn derive_validation_error_type(_input: TokenStream) -> TokenStream {
     TokenStream::new()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(ValidationErrorType)]
+    struct MyStruct {
+        field1: String,
+        field2: String,
+    }    
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }    
+}
