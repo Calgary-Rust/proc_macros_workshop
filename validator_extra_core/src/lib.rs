9840c8d386217f104d5318725db61662b2c2b321
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-04-15 15:07:12 -0600

    Demonstrated how to do testing

--- validator_extra_core/src/lib.rs
+++ validator_extra_core/src/lib.rs
@@ -1,9 +1,28 @@
+use proc_macro2::TokenStream;
+
+pub fn derive_validation_error_type(_input: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
 #[cfg(test)]
 mod tests {
+    use quote::quote;
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_validation_error_type() {
+        let input = quote! {
+            struct MyStruct {
+                field1: String,
+                field2: String,
+            }
+        };
+        let output = quote!();
+        // We can even do pretty diff, if we want to
+        // https://github.com/CarlKCarlK/anyinput/blob/d59d04146a8154015e21e2aaf924ee6140393073/anyinput-core/src/tests.rs#L31-L47
+        assert_eq!(
+            derive_validation_error_type(input).to_string(),
+            output.to_string(),
+        );
     }
 }
--- validator_extra_macros/src/lib.rs
+++ validator_extra_macros/src/lib.rs
@@ -1,22 +1,23 @@
 use proc_macro::TokenStream;
+use validator_extra_core as macro_core;
 
 #[proc_macro_derive(ValidationErrorType, attributes(validation_error_type))]
-pub fn derive_validation_error_type(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn derive_validation_error_type(input: TokenStream) -> TokenStream {
+    macro_core::derive_validation_error_type(input.into()).into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[derive(ValidationErrorType)]
-    struct MyStruct {
-        field1: String,
-        field2: String,
-    }    
+//     #[derive(ValidationErrorType)]
+//     struct MyStruct {
+//         field1: String,
+//         field2: String,
+//     }    
 
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }    
-}
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }    
+// }
